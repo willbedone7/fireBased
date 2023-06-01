e0c2dda4f5a1ca795cbc3961849c6837d945c84e
Dominic Jordan <willbedone7@icloud.com>
2023-06-01 11:49:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmUl-GqKERW9nVtOgpF6ClcDJYdr-IfwM",
+    authDomain: "signup-app-51fdc.firebaseapp.com",
+    projectId: "signup-app-51fdc",
+    storageBucket: "signup-app-51fdc.appspot.com",
+    messagingSenderId: "598696994938",
+    appId: "1:598696994938:web:a5361f701389e6082c33c3",
+    measurementId: "G-4NSQ66E8NK"
+  };
+
+  signupBtn.addEventListener('click', () => {
+    const email = prompt("Enter your email:");
+    const password = prompt("Enter your password:");
+  
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signup successful, do something with the user object
+        const user = userCredential.user;
+        console.log("Signup successful:", user);
+      })
+      .catch((error) => {
+        // Signup failed, handle the error
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Signup failed:", errorCode, errorMessage);
+      });
+  });
+  

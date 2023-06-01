44a952c130b07a0d7a6a4bc76c5089c8fd9f4a8d
Dominic Jordan <willbedone7@icloud.com>
2023-06-01 13:31:04 -0500

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBmUl-GqKERW9nVtOgpF6ClcDJYdr-IfwM",
     authDomain: "signup-app-51fdc.firebaseapp.com",
@@ -7,12 +8,19 @@ const firebaseConfig = {
     appId: "1:598696994938:web:a5361f701389e6082c33c3",
     measurementId: "G-4NSQ66E8NK"
   };
-
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const analytics = firebase.analytics();
+  
+  const auth = firebase.auth();
+  
+  const signupBtn = document.getElementById('signupBtn');
   signupBtn.addEventListener('click', () => {
     const email = prompt("Enter your email:");
     const password = prompt("Enter your password:");
   
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+    auth.createUserWithEmailAndPassword(email, password)
       .then((userCredential) => {
         // Signup successful, do something with the user object
         const user = userCredential.user;
